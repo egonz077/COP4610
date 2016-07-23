eb1867fc5f04f0de3ac64895de6e39c57053c08b
Emmanuel <egonz077@fiu.edu>
2016-07-23 16:27:24 -0400

    added includes and free pages counter

--- sys_memstats.c
+++ sys_memstats.c
@@ -13,33 +13,14 @@
 ## and no part of it has been taken from any sources.
 #########################################################*/
 
-
-/* linux/mm_types.h -> represetns every physical page on the system with the page struct
- * linux/pagge-flags.h -> contains bit flags for showing locked or dirty pages in Memory
- *        _count filed store the usage count of the page
- *        kernel checks this field indirectly via page_count() function which take a page struct as a parameter
- * page structure is associated with physical pages not virtual pages
- * linux/mmzone.h -> has struct zone which describes memory zones
- * zones are pages of memory with similar properties
- * linux/gfp.h -> interface for requesting memory
- *      page manipulation funcionts page_addres, __get_free_pages
- *
- */
-
-/*
-   1.the current number of free pages (over all memory zones);                             -> linux/mmzone.h -> zone struct -> free pages
-   2.the current number of pages used by slab allocator (over all memory zones);
-   3.the current number of pages in the active list (over all memory zones);               -> linux/mmzone.h -> zone struct->active_list
-   4.the current number of pages in the inactive list (over all memory zones);             -> linux/mmzone.h -> zone struct->inactive_list
-   5.the current number of pages in the active list whose reference bits are set (over all memory zones); -> linux
-   6.the current number of pages in the inactive list whose reference bits are set (over all memory zones); ->
-   7.the cumulative number of pages moved from the active list to the inactive list (since the last machine boot);
-   8.the cumulative number of pages evicted from the inactive list (since the last machine boot);
- */
-
-
 #include <linux/page-flags.h>
 #include <linux/mmzone.h>
+#include <linux/linkage.h>
+#include <linux/kernel.h>
+#include <linux/sched.h>
+#include <linux/types.h>
+#include <linux/memstats.h>
+
 
 struct memstats
 
@@ -48,6 +29,7 @@ asmlinkage long sys_memstats(struct memstats* info){
         // structs
         struct zone* zone;
         struct page* page;
+        struct memstats* temp;
 
         struct list_head* list;
         struct list_head* free_pages;
@@ -55,6 +37,9 @@ asmlinkage long sys_memstats(struct memstats* info){
         struct list_head* inactive;
         long unsigned int area_length;
         int i;
+
+        //allocating memory for the temporary memstats struct
+        temp = (struct memstats*)kmalloc(sizeof(struct memstats), GFP_KERNEL);
 
         /*Iterate through every zone */
         for_each_zone(zone){
@@ -66,17 +51,26 @@ asmlinkage long sys_memstats(struct memstats* info){
                         if(zone->free_area[i]) {
                                 list_for_each(free_pages, &((zone->free_area[i])->free_list)){
 
-                                        info->num_free_pages++;
+                                        temp->num_free_pages++;
 
                                 }
                         }
                 }
 
-                
-
-
 
 
         }
+
+        //copying back to user space
+        if(copy_to_user(info,temp,sizeof(struct memstats))) {
+                return -EFAULT;
+        }
+        else{
+                return 0;
+        }
+}
+
+
+}
 
 }
